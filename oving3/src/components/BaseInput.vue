037139eac0cc6d8cbb0c93ef18b826238f802b15
eltalstad <eleertalstad@gmail.com>
2023-02-15 14:36:48 +0100

    Created BaseInput component and CalculatorView.
    Small changes to main.js

---
+++ oving3/src/components/BaseInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      :value="modelValue"
+      :placeholder="placeholder"
+      :type="type"
+      :required="required"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    required: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+</style>
---
+++ oving3/src/views/CalculatorView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <CalculatorApp></CalculatorApp>
+  </div>
+</template>
+<script>
+import CalculatorApp from "@/components/CalculatorApp.vue";
+
+export default {
+  name: "CalculatorPage",
+  components: {
+    CalculatorApp,
+  },
+};
+
+</script>
