aefc4266bc7a865dd35feb77202616b0e178c731
eltalstad <eleertalstad@gmail.com>
2023-03-01 20:37:41 +0100

    Finished oving4

---
+++ oving4/BackEnd/src/test/java/oving4/backend/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package oving4.backend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
